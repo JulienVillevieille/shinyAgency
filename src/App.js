38162d459b5cab3dc4b09394114ab39e6b3059f0
Julien Villevieille <villevieille.julien@gmail.com>
2023-11-16 17:00:00 +0100

    Finalisation de l'exemple sur les outlets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import Home from './pages/Home/'
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <div></div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 import Home from './pages/Home';
 import Survey from './pages/Survey'
 import Header from './components/Header'
+// On ajoute nos composants
+import ClientForm from './components/ClientForm'
+import FreelanceForm from './components/FreelanceForm'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +15,11 @@ ReactDOM.render(
       <Header />
       <Routes>
         <Route path="/" element ={<Home />} />
-        <Route path="/survey" element ={<Survey />} />
+        <Route path="/survey" element ={<Survey />}>
+        {/* Nous imbriquons nos composants dans survey */ }
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+        </Route>
       </Routes>
     </Router>
   </React.StrictMode>,
